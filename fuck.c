a161973fd22892ec58df02edfc39cca049215dc6
SUPERQQ <442652348@qq.com>
2016-06-10 21:36:12 +0800

    啦啦啦

---
+++ fuck.c
@@ -0,0 +1,9 @@
+//
+//  fuck.c
+//  helloLib
+//
+//  Created by Libramong on 16/4/1.
+//  Copyright © 2016年 hello. All rights reserved.
+//
+
+#include "fuck.h"
---
+++ fuck.h
@@ -0,0 +1,16 @@
+//
+//  fuck.h
+//  helloLib
+//
+//  Created by Libramong on 16/4/1.
+//  Copyright © 2016年 hello. All rights reserved.
+//
+
+#ifndef fuck_h
+#define fuck_h
+
+#include <stdio.h>
+
+void fuck();
+
+#endif /* fuck_h */
